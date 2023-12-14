f20310018968543b16fe9571c437e2dfada4e90b
HarryW217 <harrywalker217@gmail.com>
2023-12-14 18:58:59 +0000

    Completed about section

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function openTab(tabName) {
   for (tabContent of tabContents) {
     tabContent.classList.remove("active-tab");
   }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabName).classList.add("active-tab");
 }
-
-console.log(tabLinks)
