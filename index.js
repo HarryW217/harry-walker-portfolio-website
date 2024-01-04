d3034ca8430ec93cd9db4531bb63ebda2aed48c1
HarryW217 <harrywalker217@gmail.com>
2024-01-04 13:12:19 +0000

    Added about me text in about me section

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const tabLinks = document.getElementsByClassName("tab-links");
 const tabContents = document.getElementsByClassName("tab-contents");
 
 function openTab(tabName) {
-  for (tabLink of tabLinks) {
+  for (let tabLink of tabLinks) {
     tabLink.classList.remove("active-link");
   }
-  for (tabContent of tabContents) {
+  for (let tabContent of tabContents) {
     tabContent.classList.remove("active-tab");
   }
   event.currentTarget.classList.add("active-link");
